71448f33e1f3648458de4b3a24833d7671b36d6d
giwi97 <giwanthawijekoon@gmail.com>
2021-03-15 21:09:33 +0530

    vue ui configs

--- public/js/app.js
+++ public/js/app.js
@@ -1879,6 +1879,7 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js").default;
 Vue.component("example-component", __webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue").default);
+mix.disableNotifications();
 var app = new Vue({
   el: "#app"
 });
