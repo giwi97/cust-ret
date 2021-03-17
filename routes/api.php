4e30afa4e73524eaca93ece40ab2c3261300c303
giwi97 <giwanthawijekoon@gmail.com>
2021-03-17 21:43:11 +0530

    api for fetch upcomings

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Upcoming;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Upcoming Task
+
+Route::get("/upcoming", function(){
+    $upcoming = Upcoming::all();
+
+    return $upcoming;
 });
