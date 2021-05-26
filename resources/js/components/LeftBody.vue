9337ed7d913c30a074361104e7cf5d48bbe65350
giwi97 <giwanthawijekoon@gmail.com>
2021-05-26 10:51:58 +0530

    nav bar content

---
+++ resources/js/components/LeftBody.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="left">
+        <img src="../../images/hamburger.png" />
+
+        <div class="profile">
+            <h1>Hi Giwantha</h1>
+            <h2>Let's make this amazing</h2>
+        </div>
+
+        <div class="search">
+            <input placeholder="Search for projects" />
+            <img src="../../images/search.png" />
+        </div>
+
+        <div class="project">
+            <div class="a-project">
+                <div class="box-color">
+                    <img src="../../images/overlay.png" />
+                    <span>PT</span>
+                </div>
+
+                <h6>Development</h6>
+            </div>
+
+            <div class="a-project">
+                <div class="box-color">
+                    <img src="../../images/overlay.png" />
+                    <span>PT</span>
+                </div>
+
+                <h6>Design</h6>
+            </div>
+
+            <div class="a-project">
+                <div class="box-color">
+                    <img src="../../images/overlay.png" />
+                    <span>PT</span>
+                </div>
+
+                <h6>Testing</h6>
+            </div>
+
+            <div class="a-project">
+                <div class="box-color">
+                    <img src="../../images/overlay.png" />
+                    <span>PT</span>
+                </div>
+
+                <h6>Article Draft</h6>
+            </div>
+
+            <div class="a-project">
+                <div class="box-color">
+                    <img src="../../images/overlay.png" />
+                    <span>PT</span>
+                </div>
+
+                <h6>Landing Page</h6>
+            </div>
+
+            <div class="a-project">
+                <div class="box-color">
+                    <img src="../../images/plus.png" />
+                    <span>PT</span>
+                </div>
+
+                <h6>Add Project</h6>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
